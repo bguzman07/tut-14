40194b956f48c6a005e5e995bab052c32bd41be4
BGuzman1s <BGuzman1s@capetigers.com>
2025-04-24 10:45:00 -0500

    second commit

--- java/ag_cards.js
+++ java/ag_cards.js
@@ -44,3 +44,103 @@ var pokerGame = {
       return this.currentBank;
    }
 }
+
+/* Constructor function for the poker cards */
+function pokerCard(cardSuit, cardRank) {
+   this.suit = cardSuit;
+   this.rank = cardRank;
+   this.rankValue = null;
+}
+
+/* Method to reference the image source file for a card */
+pokerCard.prototype.cardImage = function() {
+   var suitAbbr = this.suit.substring(0, 1).toLowerCase();
+   return "../images/" + suitAbbr + this.rankValue + ".png";
+}
+
+/* Method to replace a card with a one from the deck */
+pokerCard.prototype.replaceFromDeck = function(pokerDeck) {
+   this.suit = pokerDeck.cards[0].suit;
+   this.rank = pokerDeck.cards[0].rank;
+   this.rankValue = pokerDeck.cards[0].rankValue;
+   pokerDeck.cards.shift();
+}
+
+/* Constructor function for poker decks */
+function pokerDeck() {
+   this.cards = new Array(52);
+
+   var suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+   var ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+
+   var cardCount = 0;
+   for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 13; j++) {
+         this.cards[cardCount] = new pokerCard(suits[i], ranks[j]);
+         this.cards[cardCount].rankValue = j+2;
+         cardCount++;
+      }
+   }
+
+   // Method to randomly sort the deck
+   this.shuffle = function() {
+      this.cards.sort(function() {
+         return 0.5 - Math.random();
+      });
+   };
+
+   // Method to deal cards from the deck into a poker hand
+   this.dealTo = function(pokerHand) {
+      for (var i = 0; i < pokerHand.cards.length; i++) {
+         pokerHand.cards[i] = this.cards.shift();
+      }
+   }
+}
+
+/* Constructor function for poker hands */
+function pokerHand(handLength) {
+   this.cards = new Array(handLength);
+}
+
+/* Return the highest ranked card in the hand */
+pokerHand.prototype.highCard = function() {
+   return Math.max.call(pokerHand,
+      this.cards[0].rankValue,
+      this.cards[1].rankValue,
+      this.cards[2].rankValue,
+      this.cards[3].rankValue,
+      this.cards[4].rankValue,
+   );
+};
+
+/* Test for the presence of a flush */
+pokerHand.prototype.hasFlush = function() {
+   var firstSuit = this.cards[0].suit;
+   return this.cards.every(function(card) {
+      return card.suit === firstSuit;
+   });
+};
+
+/* Test for the presence of a straight */
+pokerHand.prototype.hasStraight = function() {
+   this.cards.sort(function(a, b) {
+      return a.rankValue - b.rankValue;
+   });
+   return this.cards.every(function(card, i, cards) {
+      if (i > 0) {
+         return (cards[i].rankValue - cards[i-1].rankValue === 1);
+      } else {
+         return true;
+      }
+   });
+};
+
+/* Test for the presence of a straight flush */ 
+pokerHand.prototype.hasStraightFlush = function() {
+   return this.hasFlush() && this.highCard() === 14;
+};
+
+/* Test for the presence of a royal flush */
+pokerHand.prototype.hasRoyalFlush = function() {
+   return this.hasStraightFlush() && this.highCard() === 14;
+}
